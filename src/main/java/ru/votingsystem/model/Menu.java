a4bb5596aa7f9b188c2ceba5bd5504f9c63c8c62
Олег Демура <demonshine1983@gmail.com>
2019-03-27 23:16:56 +0900

    Create model.

---
+++ src/main/java/ru/votingsystem/model/Menu.java
@@ -0,0 +1,7 @@
+package ru.votingsystem.model;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Menu extends AbstractNamedEntity{
+}
---
+++ src/main/java/ru/votingsystem/model/User.java
@@ -0,0 +1,8 @@
+package ru.votingsystem.model;
+
+import javax.persistence.Entity;
+
+@Entity
+public class User extends AbstractNamedEntity{
+
+}
