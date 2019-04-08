b8e5dd0194cfa9218807aafbb786be9b46c2e556
Олег Демура <demonshine1983@gmail.com>
2019-04-08 22:17:13 +0900

    Create DB init and populate.

---
+++ src/main/resources/db/initDB.sql
@@ -0,0 +1,53 @@
+DROP TABLE IF EXISTS meals;
+DROP TABLE IF EXISTS votings;
+DROP TABLE IF EXISTS restaurant;
+DROP TABLE IF EXISTS user_roles;
+DROP TABLE IF EXISTS users;
+DROP SEQUENCE IF EXISTS global_seq;
+
+CREATE SEQUENCE global_seq START WITH 100000;
+
+CREATE TABLE users
+(
+    id         INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+    name       VARCHAR                           NOT NULL,
+    email      VARCHAR                           NOT NULL,
+    password   VARCHAR                           NOT NULL,
+    registered TIMESTAMP           DEFAULT now() NOT NULL,
+    enabled    BOOL                DEFAULT TRUE  NOT NULL
+);
+CREATE UNIQUE INDEX users_unique_email_idx ON users (email);
+
+CREATE TABLE user_roles
+(
+    user_id INTEGER NOT NULL,
+    role    VARCHAR,
+    CONSTRAINT user_roles_idx UNIQUE (user_id, role),
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+
+CREATE TABLE restaurant
+(
+    id      INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+    name    TEXT NOT NULL,
+    address TEXT NOT NULL
+);
+
+CREATE TABLE votings
+(
+    id            INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+    user_id       INTEGER                           NOT NULL,
+    restaurant_id INTEGER                           NOT NULL,
+    date_voting   TIMESTAMP           DEFAULT now() NOT NULL,
+    FOREIGN KEY (restaurant_id) REFERENCES restaurant (id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+
+CREATE TABLE meals
+(
+    id            INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+    description   TEXT    NOT NULL,
+    price         INTEGER NOT NULL,
+    restaurant_id INTEGER NOT NULL,
+    FOREIGN KEY (restaurant_id) REFERENCES restaurant (id) ON DELETE CASCADE
+);
---
+++ src/main/resources/db/populateDB.sql
@@ -0,0 +1,29 @@
+DELETE FROM user_roles;
+DELETE FROM meals;
+DELETE FROM users;
+DELETE FROM restaurant;
+ALTER SEQUENCE global_seq RESTART WITH 100000;
+
+INSERT INTO users (name, email, password) VALUES
+  ('User', 'user@yandex.ru', 'password'),
+  ('Admin', 'admin@gmail.com', 'admin');
+
+INSERT INTO user_roles (role, user_id) VALUES
+  ('ROLE_USER', 100000),
+  ('ROLE_ADMIN', 100001);
+
+INSERT INTO restaurant (name, address) VALUES
+('Москва', 'г. Москва, ул. Ленина 28'),
+('Дружба', 'г. Санкт-Петербург, ул. Ким Чен Ына 18');
+
+INSERT INTO meals (description, price, restaurant_id)
+VALUES ('Завтрак', 500, 100003),
+       ('Обед', 1000, 100003),
+       ('Ужин', 500, 100003),
+       ('Завтрак', 500, 100002),
+       ('Обед', 1000, 100002),
+       ('Ужин', 510, 100002);
+
+INSERT INTO votings (user_id, restaurant_id) VALUES
+(100000, 100003),
+(100001, 100002);
